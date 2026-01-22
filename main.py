5c89cb5fe355a4cdcda3fad1d7d2afc728c3826c
날개젖은꿀벌 <54512127+surbjun1234@users.noreply.github.com>
2026-01-23 02:31:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def send_discord(schedule_list, best_notice, current_date):
         print("WEBHOOK_DATE 환경변수가 없습니다.")
         return
 
-    description = [f"• {item}" for item in schedule_list]
+    description = join([f"• {item}" for item in schedule_list])
     notice_value = f"[{best_notice['title']}]({best_notice['link']})" if best_notice else "🔍 비슷한 학사공지를 찾지 못했습니다."
     color = 15158332 if best_notice else 8421504
 
